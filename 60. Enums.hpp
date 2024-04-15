738897227682c62749de5928096c3e4810be6e08
Dann Lopez <146545698+Danncode10@users.noreply.github.com>
2024-04-15 16:42:34 +0800

    Add files via upload

---
+++ 60. Enums.hpp
@@ -0,0 +1,13 @@
+//
+//  60. Enums.hpp
+//  Lesson 47 above
+//
+//  Created by Lester Dann Lopez on 4/15/24.
+//
+
+#ifndef _0__Enums_hpp
+#define _0__Enums_hpp
+
+#include <stdio.h>
+
+#endif /* _0__Enums_hpp */
